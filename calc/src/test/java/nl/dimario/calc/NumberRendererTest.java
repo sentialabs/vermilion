f5e5c3961b37cbd922b04435c263761b08c42cfb
di Mario <mario.pinkster@gmail.com>
2020-05-10 08:46:36 +0200

    The JUnit test for actual calculations is finished

--- calc/src/test/java/nl/dimario/calc/NumberRendererTest.java
+++ calc/src/test/java/nl/dimario/calc/NumberRendererTest.java
@@ -1,5 +1,18 @@
 package nl.dimario.calc;
 
+/**
+ * Note that the values used in these calculations have been carefully
+ * selected to yield the expected results both when you do the arithmatics
+ * manually and when using the Java language library.
+ * <p>
+ * Due to the nature of floating point arithmatics, this does not
+ * always hold true for randomly selected numbers. For instance you may expect
+ * 6.02 - 3 to equal 3.02 when in fact the result calculated via
+ * NumberRender is 3.0199999999999996
+ * <p>
+ * This is not an error in the NumberRender code but follows from the
+ * imperfections inherent in floating point arithmatics.
+ */
 
 import nl.dimario.numbercalc.NumberRenderer;
 import nl.dimario.numbercalc.ScriptExpander;
@@ -47,7 +60,7 @@ public class NumberRendererTest {
             tree = scriptExpander.parse("${n3x.21}");
             assertTrue(false);
         } catch (Exception x) {
-            assert( x.getMessage().contains("mismatched input 'x'"));
+            assert (x.getMessage().contains("mismatched input 'x'"));
         }
     }
 
@@ -88,25 +101,25 @@ public class NumberRendererTest {
             tree = scriptExpander.parse("${n.foo.bar}");
             assertTrue(false);
         } catch (ParseCancellationException x) {
-            assert( x.getMessage().contains("'.foo'"));
+            assert (x.getMessage().contains("'.foo'"));
         }
         try {
             tree = scriptExpander.parse("${n2foo}");
             assertTrue(false);
         } catch (ParseCancellationException x) {
-            assert( x.getMessage().contains("'foo'"));
+            assert (x.getMessage().contains("'foo'"));
         }
         try {
             tree = scriptExpander.parse("${n#foo.bar}");
             assertTrue(false);
         } catch (ParseCancellationException x) {
-            assert( x.getMessage().contains("'#'"));
+            assert (x.getMessage().contains("'#'"));
         }
         try {
             tree = scriptExpander.parse("${n$foo.bar}");
             assertTrue(false);
         } catch (ParseCancellationException x) {
-            assert( x.getMessage().contains("'$'"));
+            assert (x.getMessage().contains("'$'"));
         }
     }
 
@@ -120,13 +133,13 @@ public class NumberRendererTest {
             tree = scriptExpander.parse("${n((44)}");
             assertTrue(false);
         } catch (ParseCancellationException x) {
-            assert( x.getMessage().contains("')'"));
+            assert (x.getMessage().contains("')'"));
         }
         try {
             tree = scriptExpander.parse("${n((44)))}");
             assertTrue(false);
         } catch (ParseCancellationException x) {
-            assert( x.getMessage().contains("')'"));
+            assert (x.getMessage().contains("')'"));
         }
     }
 
@@ -136,7 +149,78 @@ public class NumberRendererTest {
         NumberRenderer renderer = new NumberRenderer(context);
         Number number = renderer.visit(tree);
         assertEquals(6L, number);
-        tree = scriptExpander.parse("${n(2*3)}");
+
+        tree = scriptExpander.parse("${n(2.34*3)}");
+        number = renderer.visit(tree);
+        assertEquals(7.02D, number);
+
+        tree = scriptExpander.parse("${n(2*3.01)}");
+        number = renderer.visit(tree);
+        assertEquals(6.02D, number);
+
+        tree = scriptExpander.parse("${n ( 2.34 * 3.02 ) }");
+        number = renderer.visit(tree);
+        assertEquals(7.0668D, number);
+    }
+
+    @Test
+    public void testDivision() {
+        ParseTree tree = scriptExpander.parse("${n  (  6  /  3  )  }");
+        NumberRenderer renderer = new NumberRenderer(context);
+        Number number = renderer.visit(tree);
+        assertEquals(2L, number);
+
+        tree = scriptExpander.parse("${n(7.02/3)}");
+        number = renderer.visit(tree);
+        assertEquals(2.34D, number);
+
+        tree = scriptExpander.parse("${n(12/2.4)}");
+        number = renderer.visit(tree);
+        assertEquals(5.0D, number);
+
+        tree = scriptExpander.parse("${n ( 7.0668 / 3.02 ) }");
+        number = renderer.visit(tree);
+        assertEquals(2.34D, number);
+    }
+
+    @Test
+    public void testAdditition() {
+        ParseTree tree = scriptExpander.parse("${n  (  2+  5  )  }");
+        NumberRenderer renderer = new NumberRenderer(context);
+        Number number = renderer.visit(tree);
+        assertEquals(7L, number);
+
+        tree = scriptExpander.parse("${n(7.02+3 ) }");
+        number = renderer.visit(tree);
+        assertEquals(10.02D, number);
+
+        tree = scriptExpander.parse("${n(12 + 2.4)}");
+        number = renderer.visit(tree);
+        assertEquals(14.4D, number);
+
+        tree = scriptExpander.parse("${n ( 7.0668 + 3.02 ) }");
+        number = renderer.visit(tree);
+        assertEquals(10.0868D, number);
+    }
+
+    @Test
+    public void testSubtraction() {
+        ParseTree tree = scriptExpander.parse("${n  (  2 -  5  )  }");
+        NumberRenderer renderer = new NumberRenderer(context);
+        Number number = renderer.visit(tree);
+        assertEquals(-3L, number);
+
+        tree = scriptExpander.parse("${n(7.02 - 3 ) }");
+        number = renderer.visit(tree);
+        assertEquals(4.02D, number);
+
+        tree = scriptExpander.parse("${n ( 1 - 3.36)}");
+        number = renderer.visit(tree);
+        assertEquals(-2.36D, number);
+
+        tree = scriptExpander.parse("${n ( 7.03 - 2.03 ) }");
+        number = renderer.visit(tree);
+        assertEquals(5.0D, number);
     }
 
 }
